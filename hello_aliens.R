bed1388444f89f134d82a6b2889d4ed936ba7e1a
胡桐 <Peter@TongdeMacBook-Air.local>
2020-07-29 13:03:54 +0800

    ver 1

---
+++ hello_aliens.R
@@ -0,0 +1,9 @@
+cat(
+  "Dear aliens, 
+  It is very pleased to talk to you! My name is Peter and I am a human from a 
+planet called Earth. I wish I find you well. Which planet are you from? I would 
+like to visit there if possible, and humans would like to welcome you come and
+visit our planet as well. Enjoy your day!
+
+Best Wishes, 
+Peter")
